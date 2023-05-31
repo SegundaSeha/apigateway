7c44530a3f101f197fde31c7629634deb364928a
SegundaSeha <longskebalasabas@gmail.com>
2023-05-31 12:56:48 -0700

    site 1

---
+++ site2/app/Http/Controllers/UserController.php
@@ -0,0 +1,79 @@
+<?php
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use App\Models\User;
+use App\Traits\ApiResponser;
+
+Class UserController extends Controller {
+
+use ApiResponser;
+
+private $request;
+public function __construct(Request $request){
+    $this->request = $request;
+}
+
+public function getAllUsers(){
+    $users = User::all();
+    return response()->json($users, 200);
+}
+
+
+public function index()
+{
+
+    $users = User::all();
+    return $this->successResponse($users);
+
+}
+// ADD
+public function add(Request $request ){
+
+$rules = [
+    'username' => 'required|max:150',
+    'password' => 'required|max:150',
+];
+
+$this->validate($request,$rules);
+$user = User::create($request->all());
+
+return $this->successResponse($user);
+}
+
+public function show($id)
+{
+$user = User::findOrFail($id);
+return $this->successResponse($user);
+
+}
+
+public function update(Request $request,$id)
+{
+$rules = [
+'username' => 'max:150',
+'password' => 'max:150',
+];
+$this->validate($request, $rules);
+$user = User::findOrFail($id);
+$user->fill($request->all());
+
+// if no changes happen
+if ($user->isClean()) {
+return $this->errorResponse('At least one value must
+change', Response::HTTP_UNPROCESSABLE_ENTITY);
+}
+$user->save();
+return $this->successResponse($user);
+
+}
+
+public function delete($id){
+
+$user = User::findOrFail($id);
+$user->delete();
+
+return $this->successResponse($user);
+
+}
+
+}
